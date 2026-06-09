e66d55c520d1dd6982b242c71bc6fc251ef8603b
Lucas Hutchison <Lucas.hutchison05@gmail.com>
2026-06-09 14:23:15 -0600

    Troubleshooting Setup; Created Sum Calculator to Test

--- project-1/routes/web.php
+++ project-1/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (Request $request) {
+   $a = $request->input('a');
+   $b = $request->input('b');
+   $sum = null;
+
+   if ($a !== null && $b !== null) {
+    $sum = $a + $b; 
+    
+   }
+
+   return "
+   <h1>Mini Sum Calculator</h1> 
+
+   <form method='GET'>
+   <input type='number' name='a' placeholder='Enter first number' value='$a'>
+   <input type='number' name='b' placeholder='Enter second number' value='$b'>
+   <button type='submit'>Add</button>
+   </form>
+
+   <h2>Result: " . ($sum ?? 'Please Enter Number Values') . "</h2>
+   ";
+ 
+  });
+
