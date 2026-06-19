0eb701649150412f6fe54305cc246fbe88500954
Lucas Hutchison <Lucas.hutchison05@gmail.com>
2026-06-19 15:16:23 -0600

    changed UI to run with frontend asset bundling

--- project-2/resources/views/components/layout.blade.php
+++ project-2/resources/views/components/layout.blade.php
@@ -21,10 +21,13 @@
     </style>
 </head>
 
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<!--
 <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-
+-->
 <body>
 
     
